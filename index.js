269f11bcc0f6fecbe7704ff17d42a88b1047562e
jamsed_hossen <69603195+jamsed21@users.noreply.github.com>
2022-03-12 00:12:52 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 ReactDOM.render(
-  <h1> hello world </h1>,
-  document.getElementById('root')
+  <div>
+    <h1 style={{backgroundColor: "purple" , color: "white", padding: "2rem", textAlign: 'center',borderRadius : "5rem"}}> hello this is in line css in jSX</h1>
+  </div>,
+  document.getElementById('inlineCSS')
 );
+
+const internalStyle = {
+
+      backgroundColor: "black" ,
+      color: "white",
+      padding: "1rem", 
+      textAlign: 'center',
+      borderRadius : "7rem"
+}
+
+ReactDOM.render(
+  <div>
+    <h1 style={internalStyle}> hello this is internal css in jSX</h1>
+  </div>,
+  document.getElementById('internalCSS')
+);
+
+ReactDOM.render(
+  <div>
+   <p className='con'> here i using external style sheet </p>
+    
+  </div>,
+  document.getElementById('extarnalCSS')
+);
+
