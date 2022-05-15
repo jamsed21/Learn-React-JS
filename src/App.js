dc1035abef6766dfc0bcb8adbe1a74722f4677d8
jamsed_hossen <69603195+jamsed21@users.noreply.github.com>
2022-05-16 00:48:32 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import reacbot from './components/reacbot';
+
+
+export default function App() {
+  return (
+    <div>
+      <reacbot />
+     
+    </div>
+  );
+  
+}
+
+
+
---
+++ src/components/reacbot.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const reacbot = () => {
+  return (
+    <div>reacbot</div>
+  )
+}
+
+export default reacbot
