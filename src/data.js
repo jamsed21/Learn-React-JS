474cec65f5d371e82ef709f07bb6d90e2396e9d5
jamsed_hossen <69603195+jamsed21@users.noreply.github.com>
2022-05-22 02:07:30 +0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const faqsData = [
+    {
+        id: 1,
+        title: "What is your name",
+        description: "hello! this is jamsed",
+    },
+
+    {
+        id: 2,
+        title: "What is your position of this company",
+        description: "i am CEO of this comapny",
+    },
+
+    {
+        id: 3,
+        title: "What is your salary range",
+        description: "why should i told you?",
+    },
+
+    {
+        id: 4,
+        title: "What is your village name",
+        description: "shampur, comilla",
+    },
+
+    {
+        id: 5,
+        title: "What is you wife name?",
+        description: "it's persional ",
+    },
+
+    {
+        id: 6,
+        title: "What is my name",
+        description: "hello! this is jamsed",
+    },
+]
