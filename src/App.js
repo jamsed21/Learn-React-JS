921cc04f845f6374513999275144ccd9d865a129
jamsed_hossen <69603195+jamsed21@users.noreply.github.com>
2022-06-08 17:02:31 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Card from "./components/Card";
+import car1 from './compo/card1';
 
-function APP(){
-    return <div>
-        <h1 className="heading"> Todo App</h1>
-        <Card  heading1 = {<a href="#"> this is button </a>}/>
-        <Card heading1="work two"  description= "i have to do this very fastly8" />
-        <Card heading1="work three"  description= "i have to do this very fastest5" />
-        <Card heading1="work Four"  description= "i have to do this very fastong2" />
+
+
+function App(){
+  return (
+    <div>
+      <card1 name = "card1" />
     </div>
+  )
 }
 
-export default APP;
+export default App;
